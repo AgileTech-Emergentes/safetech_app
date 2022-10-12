298b1572722c0c8ab3bbe2e0b7f3a4c4f5cac807
Joset2804 <joset2804@hotmail.com>
2022-10-12 18:46:09 -0500

    Create carousel component

--- lib/pages/home_user.dart
+++ lib/pages/home_user.dart
@@ -1,5 +1,15 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
 
 class Home_user extends StatefulWidget {
   const Home_user({Key? key}) : super(key: key);
@@ -9,12 +19,12 @@ class Home_user extends StatefulWidget {
 }
 
 class _Home_userState extends State<Home_user> {
-  
   Drawer getDrawer(BuildContext context) {
     var header = DrawerHeader(
       child: Container(
         padding: EdgeInsets.only(left: 10.0),
-        child: Column( crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
               'SafeTech',
@@ -59,8 +69,10 @@ class _Home_userState extends State<Home_user> {
         getItem(new Icon(Icons.home), "Home", "/home_user"),
         getItem(new Icon(Icons.person), "Profile", "/profile_user"),
         getItem(new Icon(Icons.add), "New Appointment", "/new_appointment"),
-        getItem(new Icon(Icons.calendar_today), "Upcoming Appointments", "/upcoming_appointments"),
-        getItem(new Icon(Icons.history), "Appointment History", "/appointment_history"),
+        getItem(new Icon(Icons.calendar_today), "Upcoming Appointments",
+            "/upcoming_appointments"),
+        getItem(new Icon(Icons.history), "Appointment History",
+            "/appointment_history"),
         getItem(new Icon(Icons.logout), "Logout", "/login"),
       ],
     );
@@ -76,8 +88,57 @@ class _Home_userState extends State<Home_user> {
       appBar: AppBar(
         title: Text('Home User'),
       ),
-      body: Center(
-        child: Text('Home User'),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              CarouselSlider(
+                options: CarouselOptions(
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 3),
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  scrollDirection: Axis.horizontal,
+                ),
+                items: imgList
+                    .map((item) => Container(
+                            child: Container(
+                          margin: EdgeInsets.all(10.0),
+                          child: ClipRRect(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5.0)),
+                              child: Stack(
+                                children: <Widget>[
+                                  Image.network(item,
+                                      fit: BoxFit.cover, width: 1000.0),
+                                ],
+                              )),
+                        )))
+                    .toList(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Welcome to SafeTech',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'We are here to help you with your health',
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       drawer: new Drawer(
         child: getDrawer(context),
